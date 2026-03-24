2324ed0114ebf22b3cf8657df635cc5739a623c9
Anxcye <anxcye@outlook.com>
2026-03-24 16:24:11 +0800

    refactor: slim down resume theme

--- resume/.vuepress/config.js
+++ resume/.vuepress/config.js
@@ -1,6 +1,11 @@
 module.exports = {
   title: 'Anxcye\'s Resume',
   base: '/',
+  shouldPrefetch: () => false,
+  chainWebpack: (config) => {
+    config.plugins.delete('prefetch')
+    config.plugins.delete('prefetch-index')
+  },
   themeConfig: {
     navbar: false,
     sidebar: [{
